a70e858de8e2bd0ac8e2eb33c18e1b71544e110c
qiuzhanghua <qiuzhanghua@icloud.com>
2023-05-16 15:50:42 +0800

    add a link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,5 @@
 <p>Translated from <a href="https://dndkit.com/">dnd kit</a> for React version,</p>
 
 <p>and <a href="https://github.com/thisbeyond/solid-dnd">solid-dnd</a> for a Solid version.</p>
+
+<p>and visit <a href="https://svelte.dev/repl/adf5a97b91164c239cc1e6d0c76c2abe?version=3.14.1">Drag and Drop REPL</a></p>
